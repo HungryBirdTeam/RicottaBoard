54bddecc8469fc2b6240249dff5df289957085cc
최문경 <chl30shl20@gmail.com>
2020-10-27 16:47:18 +0900

    refactor redis structure / done

--- backend_board/src/main/java/com/websocket/board/pubsub/RedisReceiver.java
+++ backend_board/src/main/java/com/websocket/board/pubsub/RedisReceiver.java
@@ -13,23 +13,24 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 @Service
-public class RedisSubscriber implements MessageListener {
+public class RedisReceiver implements MessageListener {
+    // MessageListener : Listener of messages published in Redis
 
     private final ObjectMapper objectMapper;
     private final RedisTemplate redisTemplate;
     private final SimpMessageSendingOperations messagingTemplate;
 
     /**
-     * Redis에서 메시지가 발행(publish)되면 대기하고 있던 Subscriber가 해당 메시지를 받아 처리한다.
+     * Redis에서 메시지가 발행(publish)되면 대기하고 있던 Receiver 가 해당 메시지를 받아 처리한다.
      */
     @Override
     public void onMessage(Message message, byte[] pattern) {
         try {
-            // redis에서 발행된 데이터를 받아 deserialize
+            // redis 에서 발행된 데이터를 받아 deserialize
             String publishMessage = (String) redisTemplate.getStringSerializer().deserialize(message.getBody());
             // Board 객채로 맵핑
             SocketBoardMessage board = objectMapper.readValue(publishMessage, SocketBoardMessage.class);
-            // Websocket 구독자에게 보드 상태 Send
+            // 구독자에게 보드 상태 Send
             messagingTemplate.convertAndSend("/sub/board/channel/" + board.getId(), board);
         } catch (Exception e) {
             log.error(e.getMessage());
