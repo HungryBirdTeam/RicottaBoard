eff0752b72940c53e1e04e5fe4d7aeed9fd188d3
정진권 <jjk8060@naver.com>
2020-10-27 16:22:19 +0900

    Markdown module/Done/S03P31A204-32

--- frontend/src/components/module/Markdown.vue
+++ frontend/src/components/module/Markdown.vue
@@ -1,14 +1,12 @@
 <template>
     <div id="markdown" class="MoveableBox">
-        <div class="row changer d-flex justify-content-between">
-            <input type="text" placeholder="제목" class="col-10" v-model="title">
-            <button type="button" class="btn btn-dark col-2" @click="MarkChange" v-if="isMark">편집하기</button>
-            <button type="button" class="btn btn-dark c0l-2" @click="MarkChange" v-if="!isMark">문서보기</button>
-        </div>
-        <div class="row">
-            <textarea v-if="!isMark" :value="text" @input="update"></textarea>
-            <div class="editor" v-if="isMark" v-html="compiledMarkdown"></div>
-        </div>
+        <input type="text" placeholder="제목" class="title" v-model="title">
+        <Editor        
+            height="500px"
+            ref="toastuiEditor"
+            @change = "onEditorChange"
+            class="bg-white"
+        />
     </div>
 </template>
 
@@ -16,32 +14,30 @@
 <script src="lodash.js"></script>
 <script>
 import _ from "lodash"
+import 'codemirror/lib/codemirror.css';
+import '@toast-ui/editor/dist/toastui-editor.css';
+
+import { Editor } from '@toast-ui/vue-editor';
 
 export default {
     data() {
         return {
             title: '', 
             text: '',
-            isMark: false,
         }
+    },
+    components: {
+        Editor
     },
     props: {
         markdown: Object,
     },
-    computed: {
-        compiledMarkdown: function () {
-            const marked = require('marked');
-            console.log(this.text)
-            return marked(this.text)
-        }
-    },
     methods: {
-        update: _.debounce(function (e) {
-        this.text = e.target.value
-        }, 300),
-        MarkChange() {
-            this.isMark = !this.isMark
-        }
+        onEditorChange() {
+            var content = this.$refs.toastuiEditor.invoke("getMarkdown");
+            this.text = content
+            console.log('editor text!', this.text)
+        },
     }
 }
 </script>
@@ -49,51 +45,17 @@ export default {
 <style>
 html, body, #markdown {
     margin: 0;
-    width: 600px;
-    height: 900px;
+    width: 860px;
+    height: 610px;
     font-family: 'Helvetica Neue', Arial, sans-serif;
     color: #333;
     background-color: #f6f6f6;
 }
 
-.changer {
-    width: 600px;
+.title {
+    width: 800px;
     height: 50px;
     border: 1px solid #ccc;
     background-color: #f6f6f6;
-}
-
-.editor {
-    border: 1px solid #ccc;
-    display: inline-block;
-    vertical-align: top;
-    box-sizing: border-box;
-    width: 600px;
-    height: 850px;
-    background-color: #f6f6f6;
-}
-
-textarea {
-    border: none;
-    border-right: 1px solid #ccc;
-    border-bottom: 1px solid #ccc;
-    resize: none;
-    outline: none;
-    background-color: #f6f6f6;
-    font-size: 14px;
-    display: inline-block;
-    vertical-align: top;
-    box-sizing: border-box;
-    width: 600px;
-    height: 850px;
-    font-family: 'Monaco', courier, monospace;
-}
-
-.editor {
-    font-family: 'Helvetica Neue', Arial, sans-serif;
-}
-
-code {
-    color: #f66;
 }
 </style>
