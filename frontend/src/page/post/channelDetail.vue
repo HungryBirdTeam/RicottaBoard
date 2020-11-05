b872e8a55a3433a36a78ce6b4e3961bda1232f24
이정훈 <xorolx297@naver.com>
2020-11-05 20:17:39 +0900

    add option for socket connection / done

--- frontend/src/page/post/channelDetail.vue
+++ frontend/src/page/post/channelDetail.vue
@@ -311,7 +311,7 @@
   </div>
 </template>
 
-<script src="/socket.io/socket.io.js"></script>  
+<script src="https://k3a204.p.ssafy.io/api/facechat/socket.io/socket.io.js"></script>  
 
 <script>
 import SockJS from "sockjs-client";
