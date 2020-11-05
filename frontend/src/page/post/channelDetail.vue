96d11951527ac91704b7ee11ca06ae51f3e9ed2f
이정훈 <xorolx297@naver.com>
2020-11-05 14:16:33 +0900

    remove comment / done / S03P31A204-75

--- frontend/src/page/post/channelDetail.vue
+++ frontend/src/page/post/channelDetail.vue
@@ -472,7 +472,7 @@ export default {
       // this.board.channelId = localStorage.getItem("wsboard.channelId");
       // this.channelName = localStorage.getItem("wsboard.channelName");
       // console.log("user email",this.$store.state.userData.email);
-      loadChannelInfo(this.board.channelId, this.userEmail);
+      // loadChannelInfo(this.board.channelId, this.userEmail);
       var _this = this;
         ws.connect(
           {userNickname:this.$store.state.userData.nickname},
