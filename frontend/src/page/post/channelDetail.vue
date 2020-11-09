59b14cc8a37ff0d61e97829fa1251048a4971b7f
이정훈 <xorolx297@naver.com>
2020-11-10 01:22:04 +0900

    add client socket listener / done

--- frontend/src/page/post/channelDetail.vue
+++ frontend/src/page/post/channelDetail.vue
@@ -310,7 +310,6 @@
     <!-- <Chat /> -->
   </div>
 </template>
-<script src="https://k3a204.p.ssafy.io:3031/socket.io/socket.io.js"></script>
 
 <script>
 import SockJS from "sockjs-client";
@@ -471,6 +470,10 @@ export default {
       // var socket = new SockJS('https://localhost/api/facechat');
       // var socket = io.connect('https://k3a204.p.ssafy.io/api/facechat')
       var socket = io('https://k3a204.p.ssafy.io/api/facechat', { transports: ['websocket'] });
+
+      socket.on('connect', function () {
+        console.log("connect!!!!!!");
+      });
       // var socket = io('https://localhost:3031');
       console.log("socket", sock, socket);
       loadChannelInfo(this.board.channelId, this.userEmail, socket);
