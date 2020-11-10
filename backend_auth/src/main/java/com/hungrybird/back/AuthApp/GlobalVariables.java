2355373cb686dd1e2f9b18d29bcb68abce7489d3
Kanghyeon Kim <bebekh1216@gmail.com>
2020-11-10 17:28:08 +0900

    hotfix : modify boardPort

--- backend_auth/src/main/java/com/hungrybird/back/AuthApp/GlobalVariables.java
+++ backend_auth/src/main/java/com/hungrybird/back/AuthApp/GlobalVariables.java
@@ -4,5 +4,5 @@ public class GlobalVariables {
     public static final String host = "localhost";
     public static final String fullHost = "http://localhost";
     public static final String frontPort = ":3000";
-    public static final String boardPort = ":8080";
+    public static final String boardPort = ":8081";
 }
