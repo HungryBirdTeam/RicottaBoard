b0d5fb8441c3046ceaf5d695f69dc86bbcc66c7c
이정훈 <xorolx297@naver.com>
2020-11-11 11:23:53 +0900

    recommit

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -24,7 +24,7 @@ console.log(socket);
 // const face5 = io('http://k3a204.p.ssafy.io/api/facechat');
 // const face3 = io('http://k3a204.p.ssafy.io:3031');
 // const face4 = io('https://k3a204.p.ssafy.io:3031');
-console.log("face", face, face1, face2, face3, face4, face5, face6);
+// console.log("face", face, face1, face2, face3, face4, face5, face6);
 Vue.use(cookies);
 Vue.use(router);
 Vue.prototype.$socket = socket;
