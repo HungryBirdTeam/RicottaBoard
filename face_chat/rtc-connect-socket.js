4c6e2da25d4bff2e8aafb3dc75fd5bc0ff6598b3
이정훈 <xorolx297@naver.com>
2020-11-11 13:48:50 +0900

    http connection test

--- face_chat/rtc-connect-socket.js
+++ face_chat/rtc-connect-socket.js
@@ -4,16 +4,16 @@ const https = require('https');
 const fs = require('fs');
 const http = require('http');
 
-const options = {
-    key: fs.readFileSync('/etc/letsencrypt/live/k3a204.p.ssafy.io/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/k3a204.p.ssafy.io/cert.pem'),
-    ca: fs.readFileSync('/etc/letsencrypt/live/k3a204.p.ssafy.io/chain.pem'),
-    requestCert: false,
-    rejectUnauthorized: false
-};
+// const options = {
+//     key: fs.readFileSync('/etc/letsencrypt/live/k3a204.p.ssafy.io/privkey.pem'),
+//     cert: fs.readFileSync('/etc/letsencrypt/live/k3a204.p.ssafy.io/cert.pem'),
+//     ca: fs.readFileSync('/etc/letsencrypt/live/k3a204.p.ssafy.io/chain.pem'),
+//     requestCert: false,
+//     rejectUnauthorized: false
+// };
 
-var server = https.createServer(options, app);
-// var server = http.createServer(app);
+// var server = https.createServer(options, app);
+var server = http.createServer(app);
 
 var io = require('socket.io')(server);
 
