a162a37247774e382f9b8a34dd5405a3f33c63ea
Kanghyeon Kim <bebekh1216@gmail.com>
2020-11-12 14:40:54 +0900

    Fix : BASE_URL to AUTH

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,13 +1,13 @@
-import { BOARD_BASE_URL, createInstance } from "./index.js";
+import { AUTH_BASE_URL, createInstance } from "./index.js";
 // 보드서버
-const API_BASE_URL = BOARD_BASE_URL;
+const API_BASE_URL = AUTH_BASE_URL;
 
 const instance = createInstance(API_BASE_URL);
 
 
 function loginUser(data, success, fail) {
     instance
-        .post('/api/board/user/login', data)
+        .post('/user/login', data)
         .then(success)
         .catch(fail);
 }
